0f03998edea0de0412dd430a9fb87a5b95ca0527
Giorgio Grassini <giorgiograssini@gmail.com>
2021-05-24 16:13:45 +0200

    Updates

---
+++ precache-manifest.17138032392e406f515e247ab7b8e505.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8b23c3a2300a345f28025fa23c3b22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069e0eec4bce2d14a37a",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "a45adead1cc2dc8d3937",
+    "url": "/static/css/main.fa6b3cd0.chunk.css"
+  },
+  {
+    "revision": "069e0eec4bce2d14a37a",
+    "url": "/static/js/2.eb9d11f0.chunk.js"
+  },
+  {
+    "revision": "a45adead1cc2dc8d3937",
+    "url": "/static/js/main.28c9aced.chunk.js"
+  },
+  {
+    "revision": "f523823b3f2ad62669d8",
+    "url": "/static/js/runtime-main.ca9ccbbf.js"
+  },
+  {
+    "revision": "95dbe46b8a158b53b9439849eabf9772",
+    "url": "/static/media/about-bg.95dbe46b.png"
+  },
+  {
+    "revision": "e214b5680566862291cf60ad8f8c2eae",
+    "url": "/static/media/accordo_di_segnalazione.e214b568.docx"
+  },
+  {
+    "revision": "106a897c4281a3e56d735f7a92bbed10",
+    "url": "/static/media/accordo_mediatori.106a897c.doc"
+  },
+  {
+    "revision": "c0fb5484ec0d53569297103539fb8eb2",
+    "url": "/static/media/accordo_mediatori_en.c0fb5484.doc"
+  },
+  {
+    "revision": "6e221f8a7fa792effd5944982a00d59a",
+    "url": "/static/media/castle.6e221f8a.jpg"
+  },
+  {
+    "revision": "7c413d2b01d0ab2274c5e65ccc90722f",
+    "url": "/static/media/consenso_al_trattamento_dei_dati.7c413d2b.pdf"
+  },
+  {
+    "revision": "30e17133d78d6eb07c1e1106a9e89228",
+    "url": "/static/media/contact.30e17133.jpg"
+  },
+  {
+    "revision": "5d4e464ebc27c3fb6e38514c83875e2f",
+    "url": "/static/media/contratto_segnalatore.5d4e464e.docx"
+  },
+  {
+    "revision": "b203f255b3eabc977e1551e379424352",
+    "url": "/static/media/guida_per_acquisto_in_italia.b203f255.pdf"
+  },
+  {
+    "revision": "1dedc0ca49f028980b712ea675b8b801",
+    "url": "/static/media/key.1dedc0ca.png"
+  },
+  {
+    "revision": "89d8c9a00a32f4a7c9d417398043015e",
+    "url": "/static/media/omida-real-estate-consulting-italy.89d8c9a0.png"
+  },
+  {
+    "revision": "305f70b29544a2c31845d3f11bef20d7",
+    "url": "/static/media/view1.305f70b2.jpg"
+  },
+  {
+    "revision": "ef4b5e1181cfdf4d1a63b56b930297e4",
+    "url": "/static/media/view2.ef4b5e11.jpg"
+  },
+  {
+    "revision": "a6656231221deae20122fcf3ea216c6a",
+    "url": "/static/media/view3.a6656231.jpg"
+  }
+]);
